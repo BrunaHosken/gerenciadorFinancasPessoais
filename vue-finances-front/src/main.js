2618d994a82777c5ddf914d21101f25cf4ba39f7
BrunaHosken <bruna16.hosken@gmail.com>
2022-01-02 16:28:36 -0300

    Instalando e configurando Vuelidate

--- vue-finances-front/src/main.js
+++ vue-finances-front/src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import vuelidate from "./plugins/vuelidate";
 
 Vue.config.productionTip = false;
 
@@ -10,5 +11,6 @@ new Vue({
   router,
   store,
   vuetify,
+  vuelidate,
   render: (h) => h(App),
 }).$mount("#app");
