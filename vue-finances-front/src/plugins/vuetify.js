0dc07cb8f92a434857e0ab71b04af7b3ebd1f818
BrunaHosken <bruna16.hosken@gmail.com>
2022-01-02 16:50:08 -0300

    Ativando tema dark e customizando cores

--- vue-finances-front/src/plugins/vuetify.js
+++ vue-finances-front/src/plugins/vuetify.js
@@ -3,7 +3,6 @@ import Vuetify from "vuetify/lib";
 import pt from "vuetify/lib/locale/pt";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
-import colors from "vuetify/es5/util/colors";
 
 Vue.use(Vuetify);
 
@@ -17,11 +16,5 @@ export default new Vuetify({
   },
   theme: {
     dark: true,
-    primary: colors.teal.darken1,
-    accent: colors.indigo.darken1,
-    info: colors.blue,
-    error: colors.pink.darken2,
-    success: colors.teal.lighten1,
-    warning: colors.purple.darken1,
   },
 });
