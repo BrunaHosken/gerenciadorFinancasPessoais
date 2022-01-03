f8162a486d3ea84019c0f03167caa3eb93afc48b
BrunaHosken <bruna16.hosken@gmail.com>
2022-01-03 20:26:25 -0300

    Criando AuthService e Mutation GraphQL para login

---
+++ vue-finances-front/src/modules/auth/services/auth-service.js
@@ -0,0 +1,15 @@
+import apollo from "./../../../plugins/apollo";
+import LoginMutation from "./../graphql/Login.gql";
+
+const login = async (variables) => {
+  const response = await apollo.mutate({
+    mutation: LoginMutation,
+    variables,
+  });
+
+  return response.data.login;
+};
+
+export default {
+  login,
+};
