5b395dd6ca242fb49e2dff683df3efc25a1c2269
BrunaHosken <bruna16.hosken@gmail.com>
2022-01-03 20:37:06 -0300

    Salvando token JWT no Local Storage

--- vue-finances-front/src/modules/auth/services/auth-service.js
+++ vue-finances-front/src/modules/auth/services/auth-service.js
@@ -1,4 +1,4 @@
-import apollo from "./../../../plugins/apollo";
+import apollo, { onLogin } from "./../../../plugins/apollo";
 import LoginMutation from "./../graphql/Login.gql";
 
 const login = async (variables) => {
@@ -6,8 +6,10 @@ const login = async (variables) => {
     mutation: LoginMutation,
     variables,
   });
+  const { login } = response.data.login;
+  await onLogin(apollo, login.token);
 
-  return response.data.login;
+  return login;
 };
 
 export default {
