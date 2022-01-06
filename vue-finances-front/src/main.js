b03881cdd69f1f905047a125489863483a8e9067
BrunaHosken <bruna16.hosken@gmail.com>
2022-01-06 20:31:45 -0300

    Capturando erros globais no Vue

--- vue-finances-front/src/main.js
+++ vue-finances-front/src/main.js
@@ -2,10 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { errorHandler } from "./utils";
 import vuetify from "./plugins/vuetify";
 import vuelidate from "./plugins/vuelidate";
 
 Vue.config.productionTip = false;
+Vue.config.errorHandler = errorHandler;
 
 new Vue({
   router,
--- vue-finances-front/src/utils.js
+++ vue-finances-front/src/utils.js
@@ -1,6 +1,23 @@
+const errorHandler = (err, vm, info) => {
+  const jwtErrors = [
+    "jwt malformed",
+    "jwt expired",
+    "jwt not active",
+    "invalid token",
+  ];
+  if (jwtErrors.some((jwtError) => err.message.includes(jwtError))) {
+    vm.$router.push({
+      path: "/login",
+      query: {
+        redirect: vm.$route.path,
+      },
+    });
+  }
+};
+
 const formatError = (message) => {
   const messageSpit = message.split(":");
   return messageSpit[messageSpit.length - 1].trim();
 };
 
-export { formatError };
+export { errorHandler, formatError };
