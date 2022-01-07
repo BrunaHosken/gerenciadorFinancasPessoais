a344b5044b94c5af700293195f4b1c1f5adace7d
BrunaHosken <bruna16.hosken@gmail.com>
2022-01-06 23:17:58 -0300

    RecordsList: listando lançamentos do usuário

--- vue-finances-front/src/modules/dashboard/modules/finances/components/RecordsListItem.vue
+++ vue-finances-front/src/modules/dashboard/modules/finances/components/RecordsListItem.vue
@@ -1,16 +1,16 @@
 <template>
-  <v-list-item avatar>
+  <v-list-item>
     <v-list-item-avatar>
       <v-icon :class="[recordIconColor(record.type), 'lighten-1 white--text']">
         {{ recordIcon(record.type) }}
       </v-icon>
     </v-list-item-avatar>
-
     <v-list-item-content>
       <v-list-item-title>{{ record.description }}</v-list-item-title>
-      <v-list-item-subtitle>
-        {{ record.category.description }} | {{ record.account.description }}
-      </v-list-item-subtitle>
+      <v-list-item-subtitle
+        >{{ record.category.description }} |
+        {{ record.account.description }}</v-list-item-subtitle
+      >
     </v-list-item-content>
 
     <v-list-item-action>
@@ -28,14 +28,14 @@ export default {
   methods: {
     amountColor(amount) {
       return amount < 0
-        ? "red--text text--lighten-1"
+        ? "error--text text--lighten-1"
         : "primary--text text--lighten-1";
     },
     recordIcon(type) {
       return type === "CREDIT" ? "arrow_upward" : "arrow_downward";
     },
     recordIconColor(type) {
-      return type === "CREDIT" ? "primary" : "red";
+      return type === "CREDIT" ? "primary" : "red accent-2";
     },
   },
 };
