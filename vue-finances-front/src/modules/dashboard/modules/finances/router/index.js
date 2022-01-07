cceda6b558e43b326150430af706c6e9528c8f7f
BrunaHosken <bruna16.hosken@gmail.com>
2022-01-06 21:58:21 -0300

    Criando Finances Module e rota inicial

---
+++ vue-finances-front/src/modules/dashboard/modules/finances/router/index.js
@@ -0,0 +1,12 @@
+const RecordsHome = () => import("./../views/RecordsHome.vue");
+
+export default [
+  {
+    path: "records",
+    component: RecordsHome,
+    meta: {
+      requiresAuth: true,
+    },
+    alias: ["home", ""],
+  },
+];
