693f343751450c9cc37bd52513330e0003040f16
BrunaHosken <bruna16.hosken@gmail.com>
2022-01-07 20:21:10 -0300

    Configurando locale do Moment JS

--- vue-finances-front/src/main.js
+++ vue-finances-front/src/main.js
@@ -4,12 +4,14 @@ import router from "./router";
 import store from "./store";
 import { errorHandler } from "./utils";
 import vuetify from "./plugins/vuetify";
+import moment from "./plugins/moment";
 import vuelidate from "./plugins/vuelidate";
 
 Vue.config.productionTip = false;
 Vue.config.errorHandler = errorHandler;
 
 new Vue({
+  moment,
   router,
   store,
   vuetify,
