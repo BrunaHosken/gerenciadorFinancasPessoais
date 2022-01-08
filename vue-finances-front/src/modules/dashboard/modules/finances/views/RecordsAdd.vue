5fcc817c806863c4cf20d110c4254fe9e2749737
BrunaHosken <bruna16.hosken@gmail.com>
2022-01-08 17:01:22 -0300

    RecordsAdd: Record model e validações com Vuelidate

--- vue-finances-front/src/modules/dashboard/modules/finances/views/RecordsAdd.vue
+++ vue-finances-front/src/modules/dashboard/modules/finances/views/RecordsAdd.vue
@@ -7,20 +7,49 @@
       <v-flex xs12 sm6 md8 lg8>
         <p>Formulário</p>
       </v-flex>
+      <button @click="teste">teste</button>
     </v-layout>
   </v-container>
 </template>
 
 <script>
 import { mapActions } from "vuex";
+import moment from "moment";
+import { required, decimal, minLength } from "vuelidate/lib/validators";
 
 export default {
   name: "RecordsAdd",
+  data() {
+    return {
+      record: {
+        type: this.$route.query.type.toUpperCase(),
+        amount: 0,
+        date: moment().format("YYYY-MM-DD"),
+        accountId: "",
+        categoryId: "",
+        description: "",
+        tags: "",
+        notes: "",
+      },
+    };
+  },
+  validations: {
+    record: {
+      type: { required },
+      ammount: { required, decimal, different: (value) => value !== 0 },
+      date: { required },
+      accountId: { required },
+      categoryId: { required },
+      description: { required, minLength: minLength(6) },
+    },
+  },
   created() {
     this.changeTitle(this.$route.query.type);
   },
   beforeRouteUpdate(to, from, next) {
-    this.changeTitle(to.query.type);
+    const { type } = to.query;
+    this.changeTitle(type);
+    this.record.type = type.toUpperCase();
     next();
   },
   methods: {
@@ -38,6 +67,9 @@ export default {
           title = "Novo Lançamento";
       }
       this.setTitle({ title });
+    },
+    teste() {
+      console.log(this.record);
     },
   },
 };
