5183307491539b947b2f4772a7cbdd3cb9dd59a5
BrunaHosken <bruna16.hosken@gmail.com>
2022-01-08 16:13:03 -0300

    Floating Action Button: navegação adicional

--- vue-finances-front/src/modules/dashboard/components/AppLayout.vue
+++ vue-finances-front/src/modules/dashboard/components/AppLayout.vue
@@ -10,17 +10,21 @@
         </v-flex>
       </v-layout>
     </v-container>
+    <AppFloatingButton />
   </div>
 </template>
 
 <script>
 import AppMenu from "./AppMenu.vue";
 import AppToolbar from "./AppToolbar.vue";
+import AppFloatingButton from "./../modules/finances/components/AppFloatingButton.vue";
+
 export default {
   name: "AppLayout",
   components: {
     AppToolbar,
     AppMenu,
+    AppFloatingButton,
   },
   data: () => ({
     drawer: false,
