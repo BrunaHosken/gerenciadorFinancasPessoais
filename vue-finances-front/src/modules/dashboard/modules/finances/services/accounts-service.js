74911beae1c1be7c85d209af403b47b3b66b82ee
BrunaHosken <bruna16.hosken@gmail.com>
2022-01-09 17:21:08 -0300

    AccountsService: Mutation para criar novas Contas e reescrita do Cache

--- vue-finances-front/src/modules/dashboard/modules/finances/services/accounts-service.js
+++ vue-finances-front/src/modules/dashboard/modules/finances/services/accounts-service.js
@@ -1,6 +1,7 @@
 import apollo from "./../../../../../plugins/apollo";
 
 import AccountsQuery from "./../graphql/Accounts.gql";
+import AccountCreateMutation from "./../graphql/AccountCreate.gql";
 
 const accounts = async () => {
   const response = await apollo.query({
@@ -9,6 +10,29 @@ const accounts = async () => {
   return response.data.accounts;
 };
 
+const createAccount = async (variables) => {
+  const response = await apollo.mutate({
+    mutation: AccountCreateMutation,
+    variables,
+    update: (proxy, { data: { createAccount } }) => {
+      try {
+        const data = proxy.readQuery({
+          AccountsQuery,
+        });
+        data.accounts = [...data.accounts, createAccount];
+        proxy.writeQuery({
+          query: AccountsQuery,
+          data,
+        });
+      } catch (error) {
+        console.log('Query "acounts" has not been read yet!', error);
+      }
+    },
+  });
+  return response.data.createAccount;
+};
+
 export default {
+  createAccount,
   accounts,
 };
