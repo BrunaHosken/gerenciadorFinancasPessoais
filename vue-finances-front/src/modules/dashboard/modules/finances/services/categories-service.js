689217fbd024348a29b2515193bb603411159115
BrunaHosken <bruna16.hosken@gmail.com>
2022-01-09 17:52:03 -0300

    CategoriesService: Mutation para criar novas Categorias e reescrita do Cache

--- vue-finances-front/src/modules/dashboard/modules/finances/services/categories-service.js
+++ vue-finances-front/src/modules/dashboard/modules/finances/services/categories-service.js
@@ -1,6 +1,7 @@
 import apollo from "./../../../../../plugins/apollo";
 
 import CategoriesQuery from "./../graphql/Categories.gql";
+import CategoryCreateMutation from "./../graphql/CategoryCreate.gql";
 
 const categories = async ({ operation }) => {
   const response = await apollo.query({
@@ -9,7 +10,32 @@ const categories = async ({ operation }) => {
   });
   return response.data.categories;
 };
+const createCategory = async (variables) => {
+  const response = await apollo.mutate({
+    mutation: CategoryCreateMutation,
+    variables,
+    update: (proxy, { data: { createCategory } }) => {
+      try {
+        const variables = { operation: createCategory.operation };
+        const data = proxy.readQuery({
+          CategoriesQuery,
+          variables,
+        });
+        data.accounts = [...data.categories, createCategory];
+        proxy.writeQuery({
+          query: CategoriesQuery,
+          variables,
+          data,
+        });
+      } catch (error) {
+        console.log('Query "categories" has not been read yet!', error);
+      }
+    },
+  });
+  return response.data.createCategory;
+};
 
 export default {
   categories,
+  createCategory,
 };
