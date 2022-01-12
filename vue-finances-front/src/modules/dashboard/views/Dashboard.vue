4e79656233cdc734cc8642850941fb2cc90c6ce3
BrunaHosken <bruna16.hosken@gmail.com>
2022-01-11 21:15:47 -0300

    Vuex store, state, actions e mutations para os filtros

--- vue-finances-front/src/modules/dashboard/views/Dashboard.vue
+++ vue-finances-front/src/modules/dashboard/views/Dashboard.vue
@@ -5,11 +5,17 @@
 </template>
 
 <script>
+import { registerVuexModule } from "./../../../utils";
+import financesStore from "./../modules/finances/store";
 import AppLayout from "./../components/AppLayout.vue";
+
 export default {
   name: "Dashboard",
   components: {
     AppLayout,
+  },
+  created() {
+    registerVuexModule(this.$store, "finances", financesStore);
   },
 };
 </script>
