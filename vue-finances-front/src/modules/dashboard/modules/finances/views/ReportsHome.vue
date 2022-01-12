c5497b95acd0f27aa71e35995c43817f264b982b
BrunaHosken <bruna16.hosken@gmail.com>
2022-01-11 22:16:12 -0300

    ReportsHome: buscando Lançamentos do mês selecionado

--- vue-finances-front/src/modules/dashboard/modules/finances/views/ReportsHome.vue
+++ vue-finances-front/src/modules/dashboard/modules/finances/views/ReportsHome.vue
@@ -14,26 +14,51 @@
 
 <script>
 import { mapActions, mapState } from "vuex";
+import { Subject } from "rxjs";
+import { mergeMap } from "rxjs/operators";
 import ToolbarByMonth from "./../components/ToolbarByMonth.vue";
+import RecordsService from "./../services/records-service";
 
 export default {
   name: "ReportsHome",
   components: { ToolbarByMonth },
+  data: () => ({
+    monthSubject$: new Subject(),
+    records: [],
+    subscriptions: [],
+  }),
   computed: {
     ...mapState("finances", ["month"]),
   },
   created() {
     this.setTitle({ title: "Relatórios" });
+    this.setRecords();
+  },
+  destroyed() {
+    this.subscriptions.forEach((s) => s.unsubscribe());
   },
   methods: {
     ...mapActions(["setTitle"]),
     ...mapActions("finances", ["setMonth"]),
     changeMonth(month) {
-      this.$router.push({
-        path: this.$route.path,
-        query: { month },
-      });
+      this.$router
+        .push({
+          path: this.$route.path,
+          query: { month },
+        })
+        .catch(() => {});
       this.setMonth({ month });
+      this.monthSubject$.next(month);
+    },
+    setRecords() {
+      this.subscriptions.push(
+        this.monthSubject$
+          .pipe(mergeMap((month) => RecordsService.records({ month })))
+          .subscribe((records) => {
+            this.records = records;
+            console.log(this.records);
+          })
+      );
     },
   },
 };
